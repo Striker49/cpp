762b9a95b5ded3fd7a8c093be68e238ccd77cc95
Sebastien Roy <seroy@c2r1p7.42quebec.com>
2024-02-01 18:24:53 -0500

    PhoneBook base

---
+++ cpp00/ex01/PhoneBook.hpp
@@ -0,0 +1,19 @@
+#ifndef PHONEBOOK_HPP
+
+class   Contact {
+
+private:
+    char    firstName;
+    // char    lastName;
+    // char    nickname;
+    // char    phoneNumber;
+    // char    darkestSecret;
+};
+
+class PhoneBook {
+
+public:
+    Contact info;
+};
+
+#endif
--- cpp00/ex01/phonebook.cpp
+++ cpp00/ex01/phonebook.cpp
@@ -1,4 +1,12 @@
+#include <iostream>
+#include "PhoneBook.hpp"
+
 int main(void)
 {
+    PhoneBook   Contact1;
+
+    Contact1.info.firstName() = "Gérard";
+
+
     return (0);
 }
