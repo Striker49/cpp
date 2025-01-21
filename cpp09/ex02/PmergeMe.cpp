c02e628a98d4844898441b4fe5718821491b6bfd
Sebastien Roy <seroy@c2r2p9.42quebec.com>
2025-01-21 12:59:55 -0500

    printElements is now a template, vector is now sorted in a separate function vectorOperation()

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -37,16 +37,28 @@ int parseInput(char **argv, int argc, std::vector<int> *vec, std::deque<int> *de
 	return (0);
 }
 
-void printElements(std::vector<int> vec) {
-	size_t max = std::min(static_cast<size_t>(100), vec.size());
+template <typename T>
+void printElements(T Container) {
+	size_t max = std::min(static_cast<size_t>(100), Container.size());
 	for (size_t i = 0; i < max; i++)
-		std::cout << vec[i] << " ";
-	if (max < vec.size()) {
+		std::cout << Container[i] << " ";
+	if (max < Container.size()) {
 		std::cout << "[...] ";
-		std::cout << vec.back();
+		std::cout << Container.back();
 	}
 	std:: cout << std::endl;
 }
+
+// void printElements(std::vector<int> vec) {
+// 	size_t max = std::min(static_cast<size_t>(100), vec.size());
+// 	for (size_t i = 0; i < max; i++)
+// 		std::cout << vec[i] << " ";
+// 	if (max < vec.size()) {
+// 		std::cout << "[...] ";
+// 		std::cout << vec.back();
+// 	}
+// 	std:: cout << std::endl;
+// }
 
 void sortPairs(std::vector<std::pair<int, int> > *pairs) {
 	int temp;
@@ -81,7 +93,7 @@ void createS(std::vector<std::pair<int, int> > *pairs, std::vector<int> *S, std:
 	(*pairs).clear();
 }
 
-void createJacobsthalSequence(std::vector<int> *vec, std::vector<int> *jacob) {
+void createJacobsthalSequence(std::vector<int> vec, std::vector<int> *jacob) {
 	size_t a = 1;
 	size_t b = 1;
 	size_t temp;
@@ -89,11 +101,11 @@ void createJacobsthalSequence(std::vector<int> *vec, std::vector<int> *jacob) {
 	//push with -1 so it takes into account that count starts at 0
 	jacob->push_back(b - 1);
 	// std::cout << "vec[" << "0" << "]" << (*vec)[b] << std::endl;
-	for (size_t i = 1; i < vec->size(); i++) {
+	for (size_t i = 1; i < vec.size(); i++) {
 		temp = b;
 		b += a * 2;
 		a = temp;
-		if (b > vec->size())
+		if (b > vec.size())
 			break;
 		jacob->push_back(b - 1);
 		// std::cout << "vec[" << i << "]" << (*vec)[b] << std::endl;
@@ -115,16 +127,12 @@ std::vector<int>::iterator binarySearch(std::vector<int> *S, int item, std::vect
 	return (low);
 }
 
-void bi(std::vector<int> *S, std::vector<int> *pend) {
+void bi(std::vector<int> *S, std::vector<int> pend) {
 	
-	// size_t i;
 	std::vector<int>::iterator pos;
-	// int j;
-	// int selected;
-	std::vector<int>::iterator it = pend->begin();
-	// int n = sizeof(*pend) / sizeof((*pend)[0]);
+	std::vector<int>::iterator it = pend.begin();
 
-	while (it != pend->end()) {
+	while (it != pend.end()) {
 		std::vector<int>::iterator lowerbound = S->begin();
 		std::vector<int>::iterator upperbound = S->end();
 
@@ -140,53 +148,32 @@ void bi(std::vector<int> *S, std::vector<int> *pend) {
 }
 
 
-void binaryInsertion(std::vector<int> *S, std::vector<int> *pend, std::vector<int> *jacob) {
-	// int n = S->size();
-	// int mid;
-	// if (S->size() % 2 != 0)
-	// 	mid = n / 2 + 1;
-	// else
-	// 	mid = n / 2;
-	// int left = mid - 1;
-	// int right = mid + 1;
+void binaryInsertion(std::vector<int> *S, std::vector<int> pend, std::vector<int> *jacob) {
 	//Putting jacobsthal elements at the beginning of pend
 	// std::cout << "putting jacob elements at the beginning of pend" << std::endl;
 	while (!jacob->empty()) {
 		int index = 1;
 		// printElements(*pend);
-		(*pend).insert((*pend).begin(), (*pend)[jacob->back() - 1]);
-		// printElements(*pend);
-		(*pend).erase((*pend).begin() + jacob->back() - 1 + index, (*pend).begin() + (jacob->back() - 1 + index + 1));
+		pend.insert(pend.begin(), pend[jacob->back() - 1]);
+		// printElementspend;
+		pend.erase(pend.begin() + jacob->back() - 1 + index, pend.begin() + (jacob->back() - 1 + index + 1));
 		jacob->pop_back();
 		index++;
 		// printElements(*jacob);
 	}
 	// std::cout << "FINISHED putting jacob elements at the beginning of pend" << std::endl;
-	printElements(*pend);
-	// std::cout << "binary sorting elements of pend in S" << std::endl;
-	// while (!pend->empty())
-	// {
-		//std::cout << "Searching insertion placemement for [" << pend->front() << "]" << std::endl;
-		//std::cout << "n "  << n << std::endl;
-		//std::cout << "left "  << (*S)[left] << std::endl;
-		//std::cout << "mid "  << (*S)[mid] << std::endl;
-		//std::cout << "right "  << (*S)[right] << std::endl;
-		//printElements(*S);
+	printElements(pend);
 	bi(S, pend);
-		
-	// }
 	// std::cout << "FINISHED binary sorting elements of pend in S" << std::endl;
 }
 
-std::vector<int> *insertElements(std::vector<int> *S, std::vector<int> *pend) {
+std::vector<int> *insertElements(std::vector<int> *S, std::vector<int> pend) {
 	//insert first pend element in front of S
-	S->insert(S->begin(), (*pend)[0]);
-	pend->erase(pend->begin(), pend->begin() + 1);
+	S->insert(S->begin(), pend[0]);
+	pend.erase(pend.begin(), pend.begin() + 1);
 	std::vector<int> jacob;
 	createJacobsthalSequence(pend, &jacob);
 	binaryInsertion(S, pend, &jacob);
-	//binaryInsertion(S, pend);
-	//while (!jacob)
 	
 	return (S);
 }
@@ -199,23 +186,14 @@ bool isSorted(std::vector<int> *vec) {
 	return (true);
 }
 
-void algorithm(std::vector<int> *vec, std::deque<int> *deq) {
-	double startTime = clock();
+void vectorOperation(std::vector<int> *vec) {
+	double vecStartTime = clock();
 	std::vector<int> S;
 	std::vector<int> pend;
 	int straggler = 0;
 	int first;
 	int sec;
-	if (vec->size() < 2) {
-		std::cout << "After: ";
-		printElements(*vec);
-		std::cout << std::endl;
-		std::cout << "Time to process a range of " << vec->size() << " elements with std::vector : " << 
-		std::setprecision(6) << "0" << " µs" << std::endl;
-		std::cout << "Time to process a range of " << deq->size() << " elements with std::deque : " << 
-		std::setprecision(6) << "0" << " µs" << std::endl;
-		return;
-	}
+
 	if (vec->size() % 2 != 0)
 	{
 		straggler = vec->back();
@@ -255,24 +233,99 @@ void algorithm(std::vector<int> *vec, std::deque<int> *deq) {
 	std::cout << "Pend elements" << std::endl;
 	printElements(pend);
 
-	vec = insertElements(&S, &pend);
+	vec = insertElements(&S, pend);
+	double vecEndTime = clock();
 	std::cout << "Insert pend elements in S" << std::endl;
+	std::cout << "After: ";
 	printElements(S);
-
+	std::cout << std::endl;
+	std::cout << "Time to process a range of " << S.size() << " elements with std::vector : " << 
+	std::fixed << std::setprecision(9) << (vecEndTime - vecStartTime) / 1000000.0 << " s" << std::endl;
 	if (!isSorted(vec)) {
 		std::cout << "\033[31m" << "Container is NOT sorted!!!" << "\033[0m" << std::endl;
 		return;
 	}
 	else
 		std::cout << "\033[32m" << "Container is sorted" << "\033[0m" << std::endl;
-	double endTime = clock();
-	std::cout << "After: ";
-	printElements(*vec);
-	std::cout << std::endl;
-	std::cout << "Time to process a range of " << vec->size() << " elements with std::vector : " << 
-		std::fixed << std::setprecision(9) << (endTime - startTime) / 1000000.0 << " s" << std::endl;
-	std::cout << "Time to process a range of " << deq->size() << " elements with std::deque : " << 
-		std::fixed << std::setprecision(9) << (endTime - startTime) / 1000000.0 << " s" << std::endl;
+}
+
+// void vectorOperation(std::vector<int> *vec) {
+// 	double vecStartTime = clock();
+// 	std::vector<int> S;
+// 	std::vector<int> pend;
+// 	int straggler = 0;
+// 	int first;
+// 	int sec;
+
+// 	if (vec->size() % 2 != 0)
+// 	{
+// 		straggler = vec->back();
+// 		vec->pop_back();
+// 	}
+// 	std::vector<std::pair<int, int> > pairs;
+// 	for (std::vector<int>::iterator it = vec->begin(); it != vec->end(); it++) {
+// 		first = *it;
+// 		*it++;
+// 		sec = *it;
+// 		pairs.push_back(std::make_pair(first, sec));
+// 	}
+// 	vec->clear();
+// 	std::cout << "Created pairs" << std::endl;
+// 	for (std::vector<std::pair<int, int> >::iterator it = pairs.begin(); it != pairs.end(); it++) {
+// 		// std::cout << "pair: " << it->first << " | " << it->second << std::endl;
+// 	}
+
+// 	sortPairs(&pairs);
+// 	std::cout << "Sorted pairs bigger > smaller" << std::endl;
+// 	// for (std::vector<std::pair<int, int> >::iterator it = pairs.begin(); it != pairs.end(); it++) {
+// 	// 	std::cout << "pair: " << it->first << " | " << it->second << std::endl;
+// 	// }
+
+// 	sortPairsContainer(&pairs);
+// 	std::cout << "Sort pairs container by bigger" << std::endl;
+// 	// for (std::vector<std::pair<int, int> >::iterator it = pairs.begin(); it != pairs.end(); it++) {
+// 	// 	std::cout << "pair: " << it->first << " | " << it->second << std::endl;
+// 	// }
+
+// 	createS(&pairs, &S, &pend);
+// 	std::cout << "Put bigger in S, smaller and straggler in pend" << std::endl;
+// 	std::cout << "S elements" << std::endl;
+// 	if (straggler)
+// 		pend.push_back(straggler);
+// 	printElements(S);
+// 	std::cout << "Pend elements" << std::endl;
+// 	printElements(pend);
+
+// 	vec = insertElements(&S, pend);
+// 	double vecEndTime = clock();
+// 	std::cout << "Insert pend elements in S" << std::endl;
+// 	std::cout << "After: ";
+// 	printElements(S);
+// 	std::cout << std::endl;
+// 	std::cout << "Time to process a range of " << S.size() << " elements with std::vector : " << 
+// 	std::fixed << std::setprecision(9) << (vecEndTime - vecStartTime) / 1000000.0 << " s" << std::endl;
+// 	if (!isSorted(vec)) {
+// 		std::cout << "\033[31m" << "Container is NOT sorted!!!" << "\033[0m" << std::endl;
+// 		return;
+// 	}
+// 	else
+// 		std::cout << "\033[32m" << "Container is sorted" << "\033[0m" << std::endl;
+// }
+
+void algorithm(std::vector<int> *vec, std::deque<int> *deq) {
+	// double vecStartTime;
+	if (vec->size() < 2) {
+		std::cout << "After: ";
+		printElements(*vec);
+		std::cout << std::endl;
+		std::cout << "Time to process a range of " << vec->size() << " elements with std::vector : " << 
+		std::setprecision(6) << "0" << " µs" << std::endl;
+		std::cout << "Time to process a range of " << deq->size() << " elements with std::deque : " << 
+		std::setprecision(6) << "0" << " µs" << std::endl;
+		return;
+	}
+	vectorOperation(vec);
+	// dequeOperation(deq);
 }
 
 void compute(std::vector<int> *vec, std::deque<int> *deq) {
@@ -282,8 +335,6 @@ void compute(std::vector<int> *vec, std::deque<int> *deq) {
 	std::cout << std::endl;
 
 	algorithm(vec, deq);
-
-
 }
 
 int main(int argc, char **argv) {
